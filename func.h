9bbbdf618beca20f22d46b1fe820533e15959e27
Maksym Hedeon <maksym.hedeon@plvision.eu>
2022-06-02 17:44:04 +0300

    day3 hw

---
+++ func.h
@@ -0,0 +1,6 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+int sum(int a, int b);
+
+#endif
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "func.h"
+
+int main()
+{
+    int a = sum(1, 2);
+
+    printf("%d\n", a);
+
+    return 0;
+}
+
